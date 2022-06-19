fc527b0859b6da94840b318383e9a843e84eec62
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-19 08:32:00 +0300

    Created a function mondayWork

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function saturdayFun(arg="roller-skate"){
     return `This Saturday, I want to ${arg}`;
     }
+
+    function mondayWork(arg="go to the office") {
+        return `This Monday, I will ${arg}`;
+    }
+    
